f23ca0acaffaec888466149d035d10026a1d32d9
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-05-18 16:05:02 +0700

    add user principle class implement userdetail

--- src/main/java/com/example/quizzapp/model/User.java
+++ src/main/java/com/example/quizzapp/model/User.java
@@ -46,6 +46,9 @@ public class User {
     private String email;
 
     @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private Set<Role> roles;
 
 }
