76fc3dc17e918faf722e987c63844336d9c03d91
HoangAnhTien5396 <tienanhhoang53966@gmail.com>
2021-05-19 09:40:26 +0700

    Category

--- src/main/java/com/example/quizzapp/controller/quiz/QuizQuestionController.java
+++ src/main/java/com/example/quizzapp/controller/quiz/QuizQuestionController.java
@@ -11,6 +11,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-
+@RestController
+@RequestMapping("/question")
 public class QuizQuestionController {
+    @Autowired
+    private QuizQuestionService quizQuestionService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<QuizQuestion>> getAllQuizQuestion(Pageable pageable){
+        Page<QuizQuestion> quizQuestions = quizQuestionService.findAll(pageable);
+        return new ResponseEntity<>(quizQuestions, HttpStatus.OK);
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<QuizQuestion> getQuizQuestionById(@PathVariable("id") Long id){
+        Optional<QuizQuestion> quizQuestion = quizQuestionService.findById(id);
+        return new ResponseEntity<>(quizQuestion.get(),HttpStatus.OK);
+    }
+    @PostMapping("/create")
+    public ResponseEntity<QuizQuestion> createQuizQuestion(@RequestBody QuizQuestion quizQuestion){
+        QuizQuestion quizQuestion1 = quizQuestionService.save(quizQuestion);
+        return new ResponseEntity<>(quizQuestion1,HttpStatus.CREATED);
+    }
+    @PutMapping("/update")
+    public  ResponseEntity<QuizQuestion> updateQuizQuestion(@RequestBody QuizQuestion quizQuestion){
+        QuizQuestion quizQuestion1 = quizQuestionService.save(quizQuestion);
+        return new ResponseEntity<>(quizQuestion1,HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<QuizQuestion> deleteQuizQuestion(@PathVariable("id") Long id ){
+        quizQuestionService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
